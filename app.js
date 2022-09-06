0bdd624f50e665be3470cec516dee249cb43cc2e
Shahzar Mazhar <mohdshahzar.1996@gmail.com>
2022-09-06 19:53:45 +0530

    Fix env variable name

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(compression()); //Compress all routes
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = process.env.DB_URI;
+const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
